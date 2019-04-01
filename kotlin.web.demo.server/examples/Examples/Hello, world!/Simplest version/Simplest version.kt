0ce46380a7e023dc307ed1e21835d7735099b2fb
Krzysztof Sroga <krzysztof@sroga.pw>
2019-04-01 23:45:21 +0300

    added necessary permissions, fixed build.gradle

--- kotlin.web.demo.server/examples/Examples/Hello, world!/Simplest version/Simplest version.kt
+++ kotlin.web.demo.server/examples/Examples/Hello, world!/Simplest version/Simplest version.kt
@@ -2,7 +2,10 @@
  * We declare a package-level function main which returns Unit and takes
  * an Array of strings as a parameter. Note that semicolons are optional.
  */
-
-fun main(args: Array<String>) {
-    println("Hello, world!")
-}
+fun main() = createCode {
+    SoLangConfiguration.soLangMode = SoLangConfiguration.SoLangMode.PRINT
+    +SimpleSnippet("First revision:")
+    +StackOverflowSnippet(answerNumber = 4362605, codeBlockNumber = 2, revisionNumber = 1)
+    +SimpleSnippet("Second revision:")
+    +StackOverflowSnippet(answerNumber = 4362605, codeBlockNumber = 2, revisionNumber = 2)
+}.buildWith("python2", "script.py")
